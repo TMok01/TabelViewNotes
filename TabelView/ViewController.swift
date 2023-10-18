c73357fb91c1cf8d378fa34e45b462aaf2f73991
Tyler Mok <tymok06@gmail.com>
2023-10-18 09:06:37 -0500

    Initial Commit

---
+++ TabelView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabelView
+//
+//  Created by TYLER MOK on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
